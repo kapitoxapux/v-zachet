268ab993293baf1432ae5702381c66e4d012c7dc
kapitoxapux <kapitoxapux@gmail.com>
2020-06-09 22:27:23 +0200

    init

---
+++ resources/js/components/BasketComponent.vue
@@ -0,0 +1,30 @@
+<style>
+
+</style>
+<template>
+    <div class="fade">
+        Купить
+    </div>
+</template>
+
+<script>
+        export default {
+        data: function(){
+            return {
+                //'message_type': 'Фильм'
+            }
+        },
+        props: [
+            //'push_id',
+            //'genres'
+        ],
+        mounted() {
+            this.update();
+        },
+        methods: {
+            update: function(){
+                console.log('BasketComponent mounted!');
+            }
+        }
+    }
+</script>
